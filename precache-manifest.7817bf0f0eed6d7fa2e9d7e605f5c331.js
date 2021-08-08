d23a99dd0674764eaf05059c33dc188f518e1039
KaranamTejen <Karanam.Tejendhar@mastercard.com>
2021-08-08 22:12:22 +0530

    Updates

---
+++ precache-manifest.7817bf0f0eed6d7fa2e9d7e605f5c331.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de717e1415dc1a76d96847693b44c09a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "11dcc4fec9b541dabcda",
+    "url": "/portfolio/static/css/2.d0bdedc9.chunk.css"
+  },
+  {
+    "revision": "f6f5459d880c65804679",
+    "url": "/portfolio/static/css/main.b8207e96.chunk.css"
+  },
+  {
+    "revision": "11dcc4fec9b541dabcda",
+    "url": "/portfolio/static/js/2.832571d7.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/portfolio/static/js/2.832571d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f5459d880c65804679",
+    "url": "/portfolio/static/js/main.ee516cd7.chunk.js"
+  },
+  {
+    "revision": "b7ef451d1975e77c205b",
+    "url": "/portfolio/static/js/runtime-main.27bc9a23.js"
+  },
+  {
+    "revision": "a5742cd061d1c31a4d615ebfa07cab23",
+    "url": "/portfolio/static/media/resume.a5742cd0.pdf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b1fad0917d70b06a61bd2802d3b0f4a8",
+    "url": "/portfolio/static/media/ricknmorty.b1fad091.png"
+  }
+]);
